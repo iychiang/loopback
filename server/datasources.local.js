d3ea05051f20f9e6649dfb2a37729d8c9f00bbe6
Irene <iychiangsd@gmail.com>
2018-06-25 12:56:22 -0700

    initial

---
+++ server/datasources.local.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'name': 'MongoDB',
+    'localStorage': '',
+    'file': '',
+    'connector': 'mongodb',
+  },
+};
